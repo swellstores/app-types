5acf5c68c20326e38b123625647a542fa18e5daf
dminch <dminch@gmail.com>
2026-04-15 13:18:31 +0300

    func timeout field

--- index.ts
+++ index.ts
@@ -22,6 +22,12 @@ export interface SwellConfig {
   cron?: {
     schedule: string;
   };
+  /**
+   * Max time in ms to wait for the function worker to respond. 1000–20000.
+   * Default 10000. Values above 10000 require the `functions.long_timeout`
+   * feature.
+   */
+  timeout?: number;
 }
 
 export interface SwellStore {
